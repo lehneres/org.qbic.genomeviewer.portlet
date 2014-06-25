a9c46833dfb59d8de40d84c26bde2f9c288dd0a7
lehneres <lehneres@outlook.de>
2014-06-25 08:13:04 +0200

    liferay

--- src/org/qbic/genomeviewer/portlet.java
+++ src/org/qbic/genomeviewer/portlet.java
@@ -1,11 +1,13 @@
 package org.qbic.genomeviewer;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 
 /**
  * Created by LehnereS on 14.06.2014.
  */
+@Theme( "liferay" )
 public class portlet extends UI {
 
     @Override
